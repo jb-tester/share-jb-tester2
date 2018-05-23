e0dd2dab0a35d3ad2ba31576df61e6e79aa15b97
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-05-23 13:08:50 +0300

    Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        for (String arg : args) {
+            System.out.println(arg);
+        }
     }
 }
